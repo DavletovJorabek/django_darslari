5889fea5ef6fd0be67359f5355843c0547b007a7
DavletovJorabek <jdavletov3089@gmail.com>
2021-08-05 10:54:39 +0500

    django

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
-from django.urls import  path
+from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.hello),
-
+    path('', views.hello, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 
 ]
